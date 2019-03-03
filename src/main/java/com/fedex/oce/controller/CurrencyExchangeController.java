ab12b6de99b090cce4ade44e1f25748cd717032d
Gopikrishna Mandapati <mandapati.g.c@gmail.com>
2019-03-03 12:43:44 -0500

    Update

--- src/main/java/com/fedex/oce/controller/CurrencyExchangeController.java
+++ src/main/java/com/fedex/oce/controller/CurrencyExchangeController.java
@@ -1,7 +1,7 @@
 package com.fedex.oce.controller;
 
-import java.math.BigDecimal;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +13,8 @@ import com.fedex.oce.entity.ExchangeValue;
 @RestController
 public class CurrencyExchangeController {
 
+	private final static Logger LOG = LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@Autowired
 	private Environment env;
 	
@@ -22,6 +24,7 @@ public class CurrencyExchangeController {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue getExchangeValue(@PathVariable String from, @PathVariable String to) {
 		ExchangeValue ev = evRepo.findByFromAndTo(from, to);
+		LOG.info("{}", ev);
 		//ExchangeValue ev = new ExchangeValue(1l, from, to, new BigDecimal(65));
 		ev.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		return ev;
